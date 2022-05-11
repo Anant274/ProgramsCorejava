bb9d7d1a9f6191d5ada9f75c9eec6b30120ebfbd
Anant274 <pandeyanant274@gmail.com>
2022-05-11 17:04:29 +0530

    one to many

---
+++ OneToManyHibernateProgram/src/main/java/one_to_many/OneToManyHibernateProgram/App.java
@@ -0,0 +1,52 @@
+package one_to_many.OneToManyHibernateProgram;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        
+        Configuration cfg = new Configuration();
+		cfg.configure("one_to_many/OneToManyHibernateProgram/Config.xml");
+		SessionFactory fact = cfg.buildSessionFactory();
+
+		Session se = fact.openSession();
+		Transaction txn = null;
+
+		try {
+			System.out.println("App.main()");
+			txn = se.beginTransaction();
+			//Product prod= new Product("electronic", "tv+mobile",23000.00F );
+			
+			Product prod= new Product("electronic");
+			
+             Category ct1= new Category("mobile");
+             Category ct2= new Category("tv");
+              se.save(ct1);
+              se.save(ct2);
+              
+              List<Category> cat1= new ArrayList<Category>();
+              cat1.add(ct1);
+              cat1.add(ct2);
+              prod.setCategory(cat1);
+              se.save(prod);
+
+			se.getTransaction().commit();
+			se.close();
+
+		} catch (Exception e) {
+			txn.rollback();
+			System.out.println("record is not inserted");
+		}
+          
+    }
+}
---
+++ OneToManyHibernateProgram/src/main/java/one_to_many/OneToManyHibernateProgram/Category.java
@@ -0,0 +1,57 @@
+package one_to_many.OneToManyHibernateProgram;
+
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CATEGORY1")
+
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "CATEGORY_ID")
+	private long id;
+	@Column(name = "NAME")
+	private String name;
+
+	public Category() {
+	}
+	
+	
+
+	public Category(String name) {
+		super();
+		this.name = name;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
+	}
+
+
+
+}
